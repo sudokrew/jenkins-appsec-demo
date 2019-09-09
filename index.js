d7ff0a1064c52c3a68a9e0ae77497f2b03b84336
jaywon <jason@sudokrew.com>
2019-09-08 22:59:25 -1000

    :construction: Implement secure session cookie

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
+const COOKIE_SECRET = process.env.COOKIE_SECRET || 'testcookie';
+const DOMAIN = process.env.DOMAIN || 'localhost';
+
+const cookieOptions = {
+  domain: DOMAIN,
+  httpOnly: true,
+  maxAge: 5000,
+  path: "/admin",
+  sameSite: true
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  cookieOptions.secure = true; // serve secure cookies
+}
+
+app.use(cookieParser(COOKIE_SECRET));
+app.use(session({
+  secret: COOKIE_SECRET,
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: cookieOptions
+}));
 
 app.get('/', (req, res) => {
   const msg = {
@@ -9,6 +36,10 @@ app.get('/', (req, res) => {
   return res.status(200).json(msg);
 })
 
-module.exports = app.listen(port, () => {
-  console.log(`Application started on port ${port}`);
+app.post('/login', (req, res) => {
+
+});
+
+module.exports = app.listen(PORT, () => {
+  console.log(`Application started on port ${PORT}`);
 });
