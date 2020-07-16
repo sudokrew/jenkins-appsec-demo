38b51f6ae6d67e3ab2cd08ccae9af2ad18cd5f8a
Malia Oreta <maliaoreta@gmail.com>
2020-07-16 13:28:09 -1000

    Remove unused configs

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,11 +3,6 @@ libraries {
     defaultImage = 'node:12.3.1'
   }
   npm {
-    npmRegistryScopeName = 'sudokrew'
-    npmCredentialsId = 'GH'
-    npmRunCommand = 'test:development'
-  }
-  semantic_release {
-    semanticReleaseArgs = '--dry-run'
+    npmTestCommand = 'npm run test:development'
   }
 }
