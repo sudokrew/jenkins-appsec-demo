4810140ee2152bb83c52c8a8e3cff0d39802a48b
Malia Oreta <maliaoreta@gmail.com>
2020-07-16 16:14:36 -1000

    add configs to replicate original jenkinsfile behavior

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,8 +1,21 @@
+application_environments {
+  dev {
+    npm {
+      npmTestCommand = 'npm run test:development'
+    }
+  }
+  prod {
+    npm {
+      npmTestCommand = 'npm run test:production'
+    }
+  }
+}
+
 libraries {
   docker {
     defaultImage = 'node:12.3.1'
   }
   npm {
-    npmTestCommand = 'npm run test:development'
+    auditLevel = 'moderate'
   }
 }
